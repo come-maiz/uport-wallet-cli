65dbec7ead6bf0bac3cc8d647710238dc250109c
Joel Torstensson <oed3933@gmail.com>
2017-10-26 14:20:10 -0400

    Support for tokens

--- index.js
+++ index.js
@@ -2,39 +2,60 @@ const qrcode = require('qrcode-terminal')
 const Connect = require('uport-connect').Connect
 const program = require('commander')
 
-var uriHandler = uri => qrcode.generate(uri, {small: false})
+const tokenABI = [{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
 
-var uport = new Connect('term-dapp', {uriHandler})
+let uriHandler = uri => qrcode.generate(uri, {small: false})
 
-var web3 = uport.getWeb3()
+let uport = new Connect('term-dapp', {uriHandler})
 
+let web3 = uport.getWeb3()
+
+let Token = web3.eth.contract(tokenABI)
 
 function getBalance(options) {
-  if (options.parent.token) {
-    throw new Error('Tokens not supported yet')
-  }
   web3.eth.getCoinbase((error, address) => {
     console.log('Your rinkeby address', address)
+    if (options.parent.token) {
+      tokenBalance(address)
+    } else {
+      etherBalance(address)
+    }
+  })
 
+  function etherBalance(address) {
     web3.eth.getBalance(address, (error, balance) => {
       let numEth = web3.fromWei(balance.toNumber(), 'ether')
       console.log('Balance:', numEth, 'ETH')
     })
-  })
+  }
+
+  function tokenBalance(address) {
+    let token = Token.at(options.parent.token)
+    token.balanceOf(address, (error, balance) => {
+      console.log('Balance:', balance)
+    })
+  }
 }
 
 function send(address, numToSend, options) {
   if (options.parent.token) {
-    throw new Error('Tokens not supported yet')
+    sendToken()
+  } else {
+    sendEther()
   }
-  console.log(address, numToSend)
-  web3.eth.sendTransaction({to: address, value: web3.toWei(numToSend, 'ether')}, (error, txHash) =>{
-    console.log('TxHash:', txHash)
-  })
 
-}
+  function sendEther() {
+    web3.eth.sendTransaction({to: address, value: web3.toWei(numToSend, 'ether')}, (error, txHash) =>{
+      console.log('TxHash:', txHash)
+    })
+  }
 
-function wrap(cmd, options, action) {
+  function sendToken() {
+    let token = Token.at(options.parent.token)
+    token.transfer(address, numToSend, (error, txHash) => {
+      console.log('TxHash:', txHash)
+    })
+  }
 }
 
 program
@@ -49,5 +70,5 @@ program
 
 program
   .version('0.0.1')
-  .option('-t --token <TOKEN>', 'Which token to use')
+  .option('-t --token <token-address>', 'Which token to use')
   .parse(process.argv)
